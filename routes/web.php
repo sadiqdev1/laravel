35db51e720674e69a8b84f5e1640836447e06e07
Abubakar Ibrahim <sadiqdev2030@gmail.com>
2026-03-28 02:52:20 +0100

    Add new routes for /env and /hello

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/env', function () {
+    return env('APP_KEY');
+});
+
+Route::get('/hello', function () {
+    return 'Hello World!';
+});
